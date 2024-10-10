857b31f1a2e58523bba4febccfe3976067ab75b5
leleux <9112590p@commun.ad.sncf.fr>
2024-10-10 10:02:09 +0200

    essai

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+-- Active: 1727861747808@@127.0.0.1@3306@sncf_ticketing
 require('dotenv').config(); // Charge les variables d'environnement
 
 const { Sequelize } = require('sequelize');
---
+++ views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page PHP avec Node.js</title>
+</head>
+<body>
+    <h1><?php echo "Bienvenue sur la page PHP !"; ?></h1>
+</body>
+</html>
